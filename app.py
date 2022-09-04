10c1236c1eedb54605fe39ca40ec771f7daab37e
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2022-09-04 23:47:52 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import dash
+from dash import html
+
+
+#Initialize the app
+app = dash.Dash(__name__)
+
+
+#Define the app
+app.layout = html.Div()
+
+
+#Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
